8723a6931eeb20ed1f9faccd65ea1294e1abd760
liaozhicheng <liaozhicheng@yoyodao.cc>
2021-03-23 13:51:53 +0800

    我的博客，添加组件递归

---
+++ src/views/Blog/components/RightList-test.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="right-list--test-container">
+    <RightList :list="list" @select="handleSelect"/>
+  </div>
+</template>
+
+<script>
+import RightList from "./RightList";
+export default {
+  components: {
+    RightList,
+  },
+  data() {
+    return {
+      list: [
+        { name: "a", isSelect: false },
+        { name: "b", isSelect: false },
+        {
+          name: "c",
+          isSelect: true,
+          children: [
+            { name: "c-1", isSelect: false },
+            {
+              name: "c-2",
+              isSelect: false,
+              children: [
+                { name: "c-2-1", isSelect: false },
+                { name: "c-2-2", isSelect: false },
+                { name: "c-2-3", isSelect: false },
+                { name: "c-2-4", isSelect: false },
+              ],
+            },
+            { name: "c-3", isSelect: false },
+            { name: "c-4", isSelect: false },
+          ],
+        },
+        { name: "d", isSelect: false },
+      ],
+    };
+  },
+  methods:{
+      handleSelect(item){
+          console.log(item)
+      }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/views/Blog/components/RightList.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul class="right-list-container">
+      <li v-for="(item,i) in list" :key="i">
+          <span :class="{active : item.isSelect}" @click="handleClick(item)">{{ item.name }}</span>
+          <RightList :list="item.children" @select="handleClick"/>
+      </li>
+  </ul>
+</template>
+
+<script>
+export default {
+    name:'RightList',
+    props:{
+        list:{
+            type:Array,
+            default:()=>[]
+        }
+    },
+    methods:{
+        handleClick(item){
+            this.$emit('select',item)
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@import '~@/styles/var.less';
+.right-list-container{
+    list-style: none;
+    padding: 0;
+    li{
+        line-height: 40px;
+        cursor: pointer;
+        .right-list-container{
+            margin-left: 1em;
+        }
+        .active{
+            color: @warn;
+            font-weight: bold;
+        }
+    }
+}
+</style>
