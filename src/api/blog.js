c67fff05f6aabb79a87b5c88d5c583e8d208c770
liaozhicheng <1205218001@qq.com>
2021-03-27 00:29:15 +0800

    添加博客列表页

--- src/api/blog.js
+++ src/api/blog.js
@@ -1,5 +1,13 @@
 import request from "./request";
-export async function getBlogs(page = 1, limit = 10, categoryid = -1) {
+
+
+export async function getBlogTypes(){
+    return await request.get("/api/blogtype");
+}
+/**
+ * 获取博客列表数据
+ */
+ export async function getBlogs(page = 1, limit = 10, categoryid = -1) {
   return await request.get("/api/blog", {
     params: {
       page,
@@ -9,6 +17,9 @@ export async function getBlogs(page = 1, limit = 10, categoryid = -1) {
   });
 }
 
-export async function getBlogTypes(){
-    return await request.get("/api/blogtype");
+/**
+ * 获取博客分类
+ */
+export async function getBlogCategories(){
+  return await request.get("/api/blogtype")
 }
---
+++ src/views/Blog/components/BlogList.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="blog-list-container">
+    <ul>
+      <li>
+        <div class="thumb">
+          <a href="">
+            <img src="" alt="" />
+          </a>
+        </div>
+        <div class="main">
+          <a href="">
+            <h2></h2>
+          </a>
+          <div class="aside">
+            <span> 日期 </span>
+            <span> 浏览： </span>
+            <span> 评论： </span>
+            <a href=""></a>
+          </div>
+          <div class="desc"></div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import fetchData from '@/mixins/fetchData.js'
+import { getBlogs } from '@/api/blog.js'
+export default {
+    mixins:[fetchData()],
+    methods:{
+        async fetchData(){
+            await getBlogs()
+        }
+    }
+};
+</script>
+
+<style scoped lang="less">
+@import "~@/styles/var.less";
+.blog-list-container {
+  line-height: 1.7;
+  position: relative;
+  padding: 20px;
+  overflow-y: scroll;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  scroll-behavior: smooth;
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+}
+li {
+  display: flex;
+  padding: 15px 0;
+  border-bottom: 1px solid @gray;
+  .thumb {
+    flex: 0 0 auto;
+    margin-right: 15px;
+    img {
+      display: block;
+      max-width: 200px;
+      border-radius: 5px;
+    }
+  }
+  .main {
+    flex: 1 1 auto;
+    h2 {
+      margin: 0;
+    }
+  }
+  .aside {
+    font-size: 12px;
+    color: @gray;
+    span {
+      margin-right: 15px;
+    }
+  }
+  .desc {
+    margin: 15px 0;
+    font-size: 14px;
+  }
+}
+</style>
